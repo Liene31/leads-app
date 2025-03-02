e7b3d16d467f59a4d3b9706aacd38c17a3df36f5
Liene31 <67922137+Liene31@users.noreply.github.com>
2025-03-02 17:24:05 +0100

    Added render() + savetoLocalStorage() functions

--- index.js
+++ index.js
@@ -2,11 +2,26 @@ const inputEl = document.querySelector("#input-el");
 const saveInputBtn = document.querySelector("#input-btn");
 const listEl = document.querySelector("#list-el");
 const leadsArr = [];
-console.log(leadsArr);
+
+function render(leads) {
+  let listItems = "";
+
+  for (let i = 0; i < leadsArr.length; i++) {
+    listItems += `<li><a href="${leads[i]}" target="_blank">${leads[i]}</a></li>`;
+  }
+
+  listEl.innerHTML = listItems;
+}
 
 saveInputBtn.addEventListener("click", function () {
   const inputValue = inputEl.value;
   leadsArr.push(inputValue);
+  render(leadsArr);
+  savetoLocalStorage(leadsArr);
   console.log(leadsArr);
-  listEl.innerHTML = `<li><a href="#">${inputValue}</a></li>`;
 });
+
+function savetoLocalStorage(leads) {
+  localStorage.setItem("myArr", JSON.stringify(leads));
+  localStorage.JSON.parse();
+}
