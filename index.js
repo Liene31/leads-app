4738d0ec420e09477f2ef47da23a2a16edeae51e
Liene31 <67922137+Liene31@users.noreply.github.com>
2025-03-06 16:14:58 +0100

    Removed all the code for Local Storage

--- index.js
+++ index.js
@@ -8,19 +8,11 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
 
-console.log(firebaseConfig.databaseURL);
-
 let leadsArr = [];
 const inputEl = document.querySelector("#input-el");
 const saveInputBtn = document.querySelector("#input-btn");
 const deleteBtn = document.querySelector("#delete-btn");
 const listEl = document.querySelector("#list-el");
-const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
-
-if (leadsFromLocalStorage) {
-  leadsArr = leadsFromLocalStorage;
-  render(leadsArr);
-}
 
 function render(leads) {
   let listItems = "";
@@ -35,12 +27,10 @@ function render(leads) {
 saveInputBtn.addEventListener("click", function () {
   leadsArr.push(inputEl.value);
   inputEl.value = "";
-  localStorage.setItem("myLeads", JSON.stringify(leadsArr));
   render(leadsArr);
 });
 
 deleteBtn.addEventListener("click", function () {
-  localStorage.clear();
   leadsArr = [];
   render(leadsArr);
 });
